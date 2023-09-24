9eee75214721e7d509d42a00877902fd4da64cbc
kdrbipul <kdrbipul@gmail.com>
2023-09-24 22:09:47 +0600

    and operator check:

---
+++ main.js
@@ -0,0 +1,8 @@
+const firstName = "Abdul Kader";
+const age = 23;
+
+if(firstName[0] === 'A' && age>18){
+    console.log('Name start with A and above 18');
+}else{
+    console.log("You are Hacker!");
+}
