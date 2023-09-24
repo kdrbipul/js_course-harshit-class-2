cc6196b94d27259ce0805255c933d988e047b14d
kdrbipul <kdrbipul@gmail.com>
2023-09-24 23:43:56 +0600

    if else if

--- main.js
+++ main.js
@@ -1,24 +1,38 @@
-const firstName = "Abdul Kader";
-const age = 23;
+// const firstName = "Abdul Kader";
+// const age = 23;
 
 // if(firstName[0] === 'A' && age>18){
 //     console.log('Name start with A and above 18');
 // }else{
 //     console.log("You are Hacker!");
 // }
-if(firstName[0] === 'a' || age>18){
-    console.log('You are a right person');
+// if(firstName[0] === 'a' || age>18){
+//     console.log('You are a right person');
+// }else{
+//     console.log('You are not a valid person');
+// }
+
+
+// const winningNumber = 19;
+// const userNumber = +prompt('Gauss your number');
+// if(userNumber === winningNumber){
+//     console.log("You gauss is right");
+// }else if(userNumber < winningNumber){
+//     console.log("Number is too low");
+// }else if(userNumber > winningNumber){
+//     console.log("Number is High");
+// }
+
+
+let tempInDegree = 23;
+if(tempInDegree < 0){
+    console.log("extremly cold outside");
+}else if(tempInDegree < 25){
+    console.log("It is cold outside");
+}else if(tempInDegree < 35){
+    console.log("Wheather is okay");
+}else if(tempInDegree < 45){
+    console.log("lets go for swim");
 }else{
-    console.log('You are not a valid person');
-}
-
-
-const winningNumber = 19;
-const userNumber = +prompt('Gauss your number');
-if(userNumber === winningNumber){
-    console.log("You gauss is right");
-}else if(userNumber < winningNumber){
-    console.log("Number is too low");
-}else if(userNumber > winningNumber){
-    console.log("Number is High");
+    console.log("too hot");
 }
